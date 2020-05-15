0df0ddd2ae41477b6922065640407bbae1cf449b
rizgeek <65269115+rizgeek@users.noreply.github.com>
2020-05-15 08:14:45 +0700

    Update KNN.php

--- KNN.php
+++ KNN.php
@@ -6,59 +6,51 @@
  */
 class KNN
 {
-		public function koneksi()
-		{
-			$koneksi = mysqli_connect("localhost", "root", "", "data_mining");
-			return $koneksi;
-		}
+    public function koneksi()
+    {
+        $koneksi = mysqli_connect("localhost", "root", "", "data_mining");
+        return $koneksi;
+    }
 
-		public function data()
-		{
-			$data = mysqli_query($this->koneksi(), "SELECT * FROM knn");
-			return $data;
-		}
+    public function data()
+    {
+        $data = mysqli_query($this->koneksi(), "SELECT * FROM knn");
+        return $data;
+    }
 
-		public function squareDistance($data,$cari)
-		{
-			$squareDistance = array();
-			$index = 0;
-			foreach ($data as $value) {
-				$tmp = sqrt(pow(($value["x"] - $cari["x"]),2) + pow(($value["y"] - $cari["y"]),2) +pow(($value["z"] - $cari["z"]),2) );
-				$squareDistance[$index++] = ["distance" => $tmp, "class" => $value["kelas"]];
-			}
-			return $squareDistance;
-		}
+    public function squareDistance($data, $cari)
+    {
+        $squareDistance = array();
+        $index = 0;
+        foreach ($data as $value) {
+            $tmp = sqrt(pow(($value["x"] - $cari["x"]), 2) + pow(($value["y"] - $cari["y"]), 2) + pow(($value["z"] - $cari["z"]), 2));
+            $squareDistance[$index++] = ["distance" => $tmp, "class" => $value["kelas"]];
+        }
+        return $squareDistance;
+    }
 
-		public function sortAndCountValue($data,$kValue)
-		{	$GOOD = 0;
-			$BAD = 0;
-			sort($data);
-			for($i = 0; $i< $kValue; $i++){
-				if($data[$i]["class"] == "GOOD"){
-					$GOOD++;
-				}else{
-					$BAD++;
-				}
-			}
+    public function sortAndCountValue($data, $kValue)
+    {
+        $GOOD = 0;
+        $BAD = 0;
+        sort($data);
+        for ($i = 0; $i < $kValue; $i++) {
+            if ($data[$i]["class"] == "GOOD") {
+                $GOOD++;
+            } else {
+                $BAD++;
+            }
+        }
 
-			if($GOOD>$BAD){
-				echo "GOOD";
-			}else{
-				echo "BAD";
-			}
-			
-		}
+        if ($GOOD > $BAD) {
+            echo "GOOD";
+        } else {
+            echo "BAD";
+        }
+
+    }
 }
 
 $cari = array("x" => 1, "y" => 1, "z" => 3);
 $knn = new KNN();
-$knn->sortAndCountValue($knn->squareDistance($knn->data(), $cari),3);
-
-
-
-
-
-
-
-
-
+$knn->sortAndCountValue($knn->squareDistance($knn->data(), $cari), 3);
